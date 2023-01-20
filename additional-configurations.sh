b5d0b86abaa33428d6628436226a51c75ae151ee
FabianMonjaraz <d.fabian.monjaraz@gmail.com>
2023-01-20 10:17:25 -0600

    Added pull scripts module

--- additional-configurations.sh
+++ additional-configurations.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 bashrc_configurations() {
   if [[ ! $(which pcregrep) ]]; then
-    echo "=== Alert: pcre is not installed, additional .bashrc configurations may need to be setted manually" 
-    return 0
+    echo "=== Alert: pcregrep not found; additional .bashrc configurations may need to be setted manually" 
+    return 1
   fi
   SEARCH_PATTERN='configFolder=.*\nif \[ -f "\$\{configFolder\}/bash-manager" \]; then' 
   TARGET_FILE=${HOME}/.bashrc
@@ -25,4 +25,34 @@ EOM
   fi
 }
 
+clone_scripts() {
+  if [[ ! $(which git) ]]; then
+    echo "=== Alert: git not found; couldn't get the scripts project" 
+    return 1
+  fi
+
+  echo "=== Pulling scripts from github"
+  PROJECT="https://github.com/FabianMonjaraz/linux-scripts.git"
+  PROJECT_DIR=$(awk -F/ '{print $NF}' <<< ${PROJECT%.*})
+  CURRENT=$(pwd)
+
+  [[ ! -d ${HOME}/git ]] && mkdir -v ${HOME}/git
+  [[ ! -d ${HOME}/scripts ]] && mkdir -v ${HOME}/scripts
+
+  cd ${HOME}/git
+  if [[ ! -d ${PROJECT_DIR} ]]; then
+    git clone $PROJECT
+    RC=$?
+    [[ $RC -ne 0 ]] && return $RC
+  fi
+
+  echo "=== Linking scripts"
+  cd $PROJECT_DIR
+  for script in $(ls ./*); do
+    ln -s -v -t ${HOME}/scripts $(realpath $script)
+  done
+  cd $CURRENT
+}
+
 bashrc_configurations
+clone_scripts
