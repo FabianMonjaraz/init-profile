d4cbadd9458b6501e1316ea3c648f385cfe11681
FabianMonjaraz <d.fabian.monjaraz@gmail.com>
2023-02-13 15:11:41 -0600

    - Organized config. files location - Changed progress messages format

--- init.sh
+++ init.sh
@@ -2,17 +2,21 @@
 FILES_PATH=$(cd -- "$(dirname -- ${BASH_SOURCE[0]})" &>/dev/null && pwd)
 while read LOCATION; do
   IFS=: read ORIGIN DEST <<< $LOCATION
+  ORIGIN=files/${ORIGIN}
   # eval is used to expand the $HOME or ~ variables inside $DEST
   eval echo "=== Copy data from ${ORIGIN%\*} to $DEST"
   eval [[ ! -d $DEST ]] && mkdir -v -p $DEST
   if [[ $ORIGIN =~ .*\* ]]; then
-    # Whem ussing eval and find together, if -exec is used '\;' must be escaped
+    # When ussing eval and find together, if -exec is used with '\;' it must be escaped
     # like this: \\\;
     eval find ${FILES_PATH}/${ORIGIN%\*} -maxdepth 1 -mindepth 1 -exec cp -v -r {} $DEST \\\;
   else
     eval cp -v -r ${FILES_PATH}/${ORIGIN} $DEST
   fi
   echo
-done < ./locations
+done < locations
 
 [[ -f additional-configurations.sh ]] && bash additional-configurations.sh
+
+
+echo -e "\n=== All the personal configurations has been setted. Please, restart your session."
